ed1ed98790f20d1ab3d0088bf41d4e92446f051f
Psnokiseki <psnokiseki@PsnokisekitekiMac.local>
2014-05-07 11:22:04 +0800

    Initial Commit

---
+++ GitUseExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitUseExample
+//
+//  Created by Psnokiseki on 14-5-7.
+//  Copyright (c) 2014年 Psnokiseki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
